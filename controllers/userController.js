07b748193323949ce5e558018f8f24d1c1988fe0
Minaz Pathan <minpathan17@gmail.com>
2026-04-27 10:16:01 +0530

    Added sorting, pagination, filter in get all users method

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,49 @@ import pool from "../config/db.js";
 
 const getallUsers = async (req, res) => {
   try {
-    const result = await pool.query("SELECT * from users");
-    res.json(result.rows);
+    let { sortBy, order, page = 1, limit = 5, keyword } = req.query;
+    page = parseInt(page);
+    limit = parseInt(limit);
+    const offset = (page - 1) * limit;
+    const allowedFields = ["id", "name", "email", "role"];
+    const finalSortBy = allowedFields.includes(sortBy) ? sortBy : "id";
+    const sortOrder = order === "desc" ? "DESC" : "ASC";
+
+    let whereClause = "";
+    let values = [];
+    let valueIndex = 1;
+    if (keyword) {
+      whereClause = `WHERE name ILIKE $${valueIndex} OR email ILIKE $${valueIndex}`;
+      values.push(`%${keyword}%`);
+      valueIndex++;
+
+      // 👉 if keyword is number → search numeric fields
+      if (!isNaN(keyword)) {
+        whereClause += `
+      OR id = $${valueIndex}
+    `;
+        values.push(parseInt(keyword));
+        valueIndex++;
+      }
+    }
+
+    const query = `
+      SELECT * FROM users
+      ${whereClause}
+      ORDER BY ${finalSortBy} ${sortOrder}
+      LIMIT $${valueIndex} OFFSET $${valueIndex + 1}
+    `;
+    values.push(limit, offset);
+
+    const result = await pool.query(query, values);
+    const countResult = await pool.query("SELECT COUNT(*) FROM users");
+    const total = parseInt(countResult.rows[0].count);
+    res.json({
+      total,
+      page,
+      limit,
+      data: result.rows,
+    });
   } catch (err) {
     res.json({ err: err.message });
   }
@@ -15,7 +56,7 @@ const updateUser = async (req, res) => {
     const { name, password, email, role } = req.body;
     const result = await pool.query(
       `update users set name=$1, password=$2, email=$3, role=$4 where id=$5 returning *`,
-      [name, password, email, role, id]
+      [name, password, email, role, id],
     );
     if (result.rows.length === 0) {
       return res.json({ msg: "User not found" });
@@ -30,10 +71,13 @@ const updateUser = async (req, res) => {
   }
 };
 
-const deleteUser = async(req, res)=>{
-   try {
+const deleteUser = async (req, res) => {
+  try {
     const id = req.params.id;
-    const result = await pool.query("Delete from users where id=$1 returning *", [id])
+    const result = await pool.query(
+      "Delete from users where id=$1 returning *",
+      [id],
+    );
     if (result.rows.length === 0) {
       return res.json({ msg: "user not found" });
     }
@@ -42,18 +86,17 @@ const deleteUser = async(req, res)=>{
       msg: "user deleted",
       data: result.rows[0],
     });
-   }
-   catch(err){
-    req.json({err: err.message})
-   }
-}
+  } catch (err) {
+    req.json({ err: err.message });
+  }
+};
 
 const getUserById = async (req, res) => {
   try {
     const id = parseInt(req.params.id);
     const result = await pool.query(
       "SELECT id, name, email, role FROM users WHERE id = $1",
-      [id]
+      [id],
     );
     if (result.rows.length === 0) {
       return res.json({ msg: "User not found" });
